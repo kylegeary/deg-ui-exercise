27ee0c8ebaf524b1f94c542df28952ef7b733fce
Kyle Geary <Kgeary09@gmail.com>
2018-08-16 13:18:40 -0500

    Fix grid column spacing and validateZipCode function

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const zipCode = document.getElementById("zipCodeField").value;
 const parsedZipCode = parseInt(zipCode);
 function validateZipCode () {
-    if (parsedZipCode.length === 5 && isNaN(parsedZipCode) == false) {
+    if ((zipCode.length == 5) && (isNaN(parsedZipCode) == false)) {
         console.log("you\'re good.");
     } else {
         console.log("That's not a number, yo.")
