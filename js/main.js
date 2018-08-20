70534f82e2e473bc2a5b6e64e2bf695f61c3ae51
Kyle Geary <Kgeary09@gmail.com>
2018-08-19 21:15:51 -0500

    Add logic to display current distance

--- js/main.js
+++ js/main.js
@@ -6,14 +6,36 @@ zipCodeInput.addEventListener('keydown', function (event) {
     }
 });
 
+const range = document.getElementById("mileRange");
+range.addEventListener("input", function (event) {
+    event.preventDefault;
+    displayDistance();
+})
+
 
 function validateZipCode() {
     let zipCode = document.getElementById("zipCodeField").value;
+    let displayDistance = document.getElementById("distanceFromZip");
+    let range = document.getElementById("mileRange").value;
     let valid = /^[0-9]{5}?$/.test(zipCode);
     if (valid){
         alert("Okay, we're cool.")
+        displayDistance.innerText = "Current: " + range + " miles from " + zipCode;
     } else {
         alert("Get your shit together, bro.")
+        displayDistance.innerText = "";
 
+    }
+}
+
+function displayDistance() {
+    let zipCode = document.getElementById("zipCodeField").value;
+    let range = document.getElementById("mileRange").value;
+    let displayDistance = document.getElementById("distanceFromZip");
+    let valid = /^[0-9]{5}?$/.test(zipCode);
+    if (valid) {
+        displayDistance.innerText = "Current: " + range + " miles from " + zipCode;
+    } else {
+    displayDistance.innerText = "";
     }
 }
