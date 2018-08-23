0b42d698e8353d969d6df44a18bbe32d63870aac
Kyle Geary <Kgeary09@gmail.com>
2018-08-23 10:16:25 -0500

    Fix JSON call

--- js/main.js
+++ js/main.js
@@ -58,279 +58,32 @@ function displayDistance() {
 
 /*Filters JSON, adds content to HTML based on filters*/
 function getDoctors() {
-    let json = {
-        "results": [
-            {
-                "fullName": "Nick Andrew Mandela",
-                "lastName": "Mandela",
-                "specialties": [
-                    "Vein Care Again",
-                    "Neurology"
-                ],
-                "url": "#",
-                "image": "assets/avatar.png",
-                "gender": "Male",
-                "bio": "",
-                "locations": [
-                    {
-                        "name": "St Louis",
-                        "url": "https://www.google.com",
-                        "streetAddress": null,
-                        "cityStateZip": null,
-                        "lat": 0,
-                        "lng": 0,
-                        "distance": 0.8122855139943341,
-                        "phone": null,
-                        "image": null,
-                        "availability": null
-                    },
-                    {
-                        "name": "Olathe",
-                        "url": "https://www.google.com",
-                        "streetAddress": null,
-                        "cityStateZip": null,
-                        "lat": 0,
-                        "lng": 0,
-                        "distance": 23.1210731921023,
-                        "phone": null,
-                        "image": null,
-                        "availability": null
-                    },
-                    {
-                        "name": "Vein Care Center",
-                        "url": "https://www.google.com",
-                        "streetAddress": null,
-                        "cityStateZip": null,
-                        "lat": 0,
-                        "lng": 0,
-                        "distance": 12.59373900731615,
-                        "phone": null,
-                        "image": null,
-                        "availability": null
-                    }
-                ]
-            },
-            {
-                "fullName": "Dr. Ryan Heap",
-                "lastName": "Heap",
-                "specialties": [
-                    "Vein Care Again",
-                    "Neurology"
-                ],
-                "url": "#",
-                "image": "assets/drheap.jpg",
-                "gender": "Male",
-                "bio": "Dr. Ryan \"Doogie\" Heap is the son of Ronnie (Harrison Ford) and JoJo Heap (Linda Hamilton). As a child, he twice survived early-stage pediatric leukemia after his father—a family physician—discovered suspicious bruising. The experience contributed to the younger Heap's desire to enter medicine.",
-                "locations": [
-                    {
-                        "name": "St Louis",
-                        "url": "https://www.google.com",
-                        "streetAddress": null,
-                        "cityStateZip": null,
-                        "lat": 0,
-                        "lng": 0,
-                        "distance": 5.22855139943341,
-                        "phone": null,
-                        "image": null,
-                        "availability": null
-                    },
-                    {
-                        "name": "Olathe",
-                        "url": "https://www.google.com",
-                        "streetAddress": null,
-                        "cityStateZip": null,
-                        "lat": 0,
-                        "lng": 0,
-                        "distance": 15.1210731921023,
-                        "phone": null,
-                        "image": null,
-                        "availability": null
-                    },
-                    {
-                        "name": "Vein Care Center",
-                        "url": "https://www.google.com",
-                        "streetAddress": null,
-                        "cityStateZip": null,
-                        "lat": 0,
-                        "lng": 0,
-                        "distance": 8.59373900731615,
-                        "phone": null,
-                        "image": null,
-                        "availability": null
-                    }
-                ]
-            },
-            {
-                "fullName": "Dr. Jan Smith",
-                "lastName": "Smith",
-                "specialties": [],
-                "url": "#",
-                "image": "assets/avatar.png",
-                "gender": "Female",
-                "bio": "",
-                "locations": [
-                    {
-                        "name": "Olathe",
-                        "url": "https://www.google.com",
-                        "streetAddress": null,
-                        "cityStateZip": null,
-                        "lat": 0,
-                        "lng": 0,
-                        "distance": 32.1210731921023,
-                        "phone": null,
-                        "image": null,
-                        "availability": null
-                    }
-                ]
-            },
-            {
-                "fullName": "John A Doe",
-                "lastName": "Doe",
-                "specialties": [],
-                "languages": [
-                    "English"
-                ],
-                "url": "#",
-                "image": "assets/avatar.png",
-                "gender": null,
-                "bio": "",
-                "locations": [
-                    {
-                        "name": "Olathe",
-                        "url": "https://www.google.com",
-                        "streetAddress": null,
-                        "cityStateZip": null,
-                        "lat": 0,
-                        "lng": 0,
-                        "distance": 12.1210731921023,
-                        "phone": null,
-                        "image": null,
-                        "availability": null
-                    }
-                ]
-            },
-            {
-                "fullName": "John D Test",
-                "lastName": "Test",
-                "specialties": [],
-                "url": "#",
-                "image": "assets/avatar.png",
-                "gender": null,
-                "bio": "",
-                "locations": [
-                    {
-                        "name": "Olathe",
-                        "url": "https://www.google.com",
-                        "streetAddress": null,
-                        "cityStateZip": null,
-                        "lat": 0,
-                        "lng": 0,
-                        "distance": 3.1210731921023,
-                        "phone": null,
-                        "image": null,
-                        "availability": null
-                    }
-                ]
-            },
-            {
-                "fullName": "Alison T Test",
-                "lastName": "Test",
-                "specialties": [
-                    "Vein Care Again",
-                    "Neurology"
-                ],
-                "url": "#",
-                "image": "assets/avatar.png",
-                "gender": "Female",
-                "bio": "",
-                "locations": [
-                    {
-                        "name": "Olathe",
-                        "url": "https://www.google.com",
-                        "streetAddress": null,
-                        "cityStateZip": null,
-                        "lat": 0,
-                        "lng": 0,
-                        "distance": 1.1210731921023,
-                        "phone": null,
-                        "image": null,
-                        "availability": null
-                    },
-                    {
-                        "name": "Vein Care Center",
-                        "url": "https://www.google.com",
-                        "streetAddress": null,
-                        "cityStateZip": null,
-                        "lat": 0,
-                        "lng": 0,
-                        "distance": 12.59373900731615,
-                        "phone": null,
-                        "image": null,
-                        "availability": null
-                    }
-                ]
-            },
-            {
-                "fullName": "Test A Test",
-                "lastName": "Test",
-                "specialties": [
-                    "Neurology"
-                ],
-                "url": "#",
-                "image": "assets/avatar.png",
-                "gender": "Female",
-                "bio": "",
-                "locations": [
-                    {
-                        "name": "Olathe",
-                        "url": "https://www.google.com",
-                        "streetAddress": null,
-                        "cityStateZip": null,
-                        "lat": 0,
-                        "lng": 0,
-                        "distance": 22.1210731921023,
-                        "phone": null,
-                        "image": "assets/avatar.png",
-                        "availability": null
-                    },
-                    {
-                        "name": "Lenexa",
-                        "url": "https://www.google.com",
-                        "streetAddress": null,
-                        "cityStateZip": null,
-                        "lat": 0,
-                        "lng": 0,
-                        "distance": 16.92244050719052,
-                        "phone": null,
-                        "image": null,
-                        "availability": null
-                    }
-                ]
-            }
-        ]
-    };
-    let range = document.querySelector("#mileRange").value;
-    let gender = document.querySelector("input[name = 'gender']:checked").value;
-    let returned = json.results;
-    if (range < 30) {
-        returned = returned.filter(i => { return i.locations.find(l => l.distance <= range) != undefined; });
-    }
-    if (gender) {
-        returned = returned.filter(i => i.gender && i.gender == gender);
-    }
-    document.getElementById("resultAmountText").innerText = "Total Results: " + returned.length;
-    let doctorsList = "";
+    fetch("doctors.json").then(function (response) {
+        return response.json();
+    }).then(function (json) {
+        let range = document.querySelector("#mileRange").value;
+        let gender = document.querySelector("input[name = 'gender']:checked").value;
+        let returned = json.results;
+        if (range < 30) {
+            returned = returned.filter(i => { return i.locations.find(l => l.distance <= range) != undefined; });
+        }
+        if (gender) {
+            returned = returned.filter(i => i.gender && i.gender == gender);
+        }
+        document.getElementById("resultAmountText").innerText = "Total Results: " + returned.length;
+        let doctorsList = "";
 
-    returned.forEach(c => {
-        let specialties = "";
-        c.specialties.forEach(s => {
-            specialties += "<li class=\"specialties\">" + s + "</li>";
+        returned.forEach(c => {
+            let specialties = "";
+            c.specialties.forEach(s => {
+                specialties += "<li class=\"specialties\">" + s + "</li>";
+            })
+            let locations = "";
+            c.locations.forEach(l => {
+                locations += "<a href=\"" + l.url + "\">" + l.name + "</a><br>" + l.distance.toFixed(0) + " miles <br>";
+            })
+            doctorsList += "<div class=\"doctorCard\"><ul class=\"doctorImages\"><li><img class=\"doctorImage\" src=\"" + c.image + "\"></li></ul><ul class=\"profileInfo\"><li class=\"fullName\">" + c.fullName + "<li><br>" + specialties + "</ul><br><ul class=\"locators\">" + locations + "</ul><br></div><hr>"
         })
-        let locations ="";
-        c.locations.forEach(l => {
-            locations +="<a href=\"" + l.url + "\">" + l.name + "</a><br>" + l.distance.toFixed(0) + " miles <br>";
-        })
-        doctorsList += "<div class=\"doctorCard\"><ul class=\"doctorImages\"><li><img class=\"doctorImage\" src=\"" + c.image + "\"></li></ul><ul class=\"profileInfo\"><li class=\"fullName\">" + c.fullName + "<li><br>" + specialties + "</ul><br><ul class=\"locators\">" + locations + "</ul><br></div><hr>"
+        document.querySelector("#doctors").innerHTML = doctorsList;
     });
-    document.querySelector("#doctors").innerHTML = doctorsList;
 };
